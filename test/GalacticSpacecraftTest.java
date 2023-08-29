91cbe8eef8b29091a3ccaf73bf7ad3898207c9e9
Aboti Akshay <aboti.akshay1125@gmail.com>
2023-08-29 18:07:30 +0530

    implementing test - move bacward below bondary

--- test/GalacticSpacecraftTest.java
+++ test/GalacticSpacecraftTest.java
@@ -56,6 +56,13 @@ public void testMoveBackwardFromStart() {
     assertEquals("(0, -1, 0) - N", spacecraft.getPositionAndDirection());
 }
 
+@Test
+public void testInvalidMovementBelowBoundary() {
+    GalacticSpacecraft spacecraft = new GalacticSpacecraft(0, 0, 0, "Down");
+    spacecraft.moveBackward();
+    assertEquals("(0, 0, -1) - Down", spacecraft.getPositionAndDirection());
+}
+
     // Add more test cases for other movement and rotation scenarios
     
     @Test
