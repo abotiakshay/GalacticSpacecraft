faf070b8a07efcffbbd3ccc10980a5f8e306a325
Aboti Akshay <aboti.akshay1125@gmail.com>
2023-08-29 18:03:45 +0530

    test boundary movement

--- test/GalacticSpacecraftTest.java
+++ test/GalacticSpacecraftTest.java
@@ -33,6 +33,15 @@ public void testInvalidMovementAboveBoundary() {
 }
 
 
+@Test
+public void testBoundaryMovement() {
+    GalacticSpacecraft spacecraft = new GalacticSpacecraft(0, 0, 0, "N");
+    spacecraft.moveForward();
+    spacecraft.moveForward();
+    spacecraft.moveBackward();
+    assertEquals("(0, 1, 0) - N", spacecraft.getPositionAndDirection());
+}
+
 
     // Add more test cases for other movement and rotation scenarios
     
